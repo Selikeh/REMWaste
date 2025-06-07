60d55484b3e9d2114ed1178806f0d5f877cf841a
Selikeh <skgborbidzi@gmail.com>
2025-06-07 13:03:05 +0000

    add info subtext

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,7 @@ import large_skip from "../assets/large-skip.png"
 import heavy_skip from "../assets/heavy-skip.png"
 import { TiWarning } from "react-icons/ti";
 import { LuTimer } from "react-icons/lu";
+import { MdOutlineWidthWide } from "react-icons/md";
 
 interface SkipData {
     allowed_on_road: boolean;
@@ -26,14 +27,14 @@ interface SkipData {
 
 function Card({skip, selected, onClick}:{skip:SkipData, selected:boolean, onClick:(skip:SkipData)=>void}) {
   return (
-    <li className={` w-fit group rounded-xl col-span-12 md:col-span-6 lg:col-span-4 hover:cursor-pointer hover:bg-darklight-green ${selected?"bg-main-green":""}`} onClick={()=>{onClick(skip)}}>                
+    <li className={` w-fit group rounded-xl col-span-12 md:col-span-6 lg:col-span-4 hover:cursor-pointer hover:bg-darklight-green ${selected?"bg-main-green/90":""}`} onClick={()=>{onClick(skip)}}>                
         <div className=' w-fit h-fit relative'>
             <img className='' src={skip.size<8?small_skip:skip.size<12?light_medium_skip:skip.size<16?medium_skip:skip.size<40?large_skip:heavy_skip} alt="" />
             <input type="radio" name="" id="" checked={selected} className=' h-6 w-6 absolute top-2 right-2 accent-main-green' />  
         </div>
         <div className={`  px-2 py-1.5 group-hover:text-slate-50 ${selected?"text-darklight-green":""}`}>
             <div className=' flex items-center justify-between'>
-            <h3 className=' text-lg font-semibold'>{skip.size} yard skip</h3>
+            <h3 className=' text-lg font-semibold flex items-center gap-1'><MdOutlineWidthWide />{skip.size} yard skip</h3>
             <h3 className=' flex items-center gap-0.5'><GiMoneyStack/><span>&#163;</span>{skip.price_before_vat}</h3>
             </div>
             <p className={` flex items-center gap-0.5 text-sm group-hover:text-slate-50 ${selected?"text-darklight-green":"text-secondary-green"}`}>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { GiWeight } from "react-icons/gi";
+import { BsInfoCircleFill } from "react-icons/bs";
 import small_skip from "../assets/small-skip.png"
 import light_medium_skip from "../assets/light-medium-skip.png"
 import medium_skip from "../assets/medium-skip.png"
@@ -24,11 +25,11 @@ interface SkipData {
 
 function Footer({selectedData}:{selectedData:SkipData}) {
   return (
-    <footer className={`fixed left-0 right-0 w-screen bg-darklight-green rounded-t-lg  flex justify-center transition-all duration-500 overflow-hidden ${selectedData.id?"bottom-0":"-bottom-[450px]"}`}>
-              <div className='  px-3 p-2 space-y-3.5 h-fit mt-2 w-5xl text-secondary-green '>
+    <footer className={`fixed left-0 right-0 w-screen bg-darklight-green rounded-t-lg  flex justify-center transition-all duration-500 overflow-hidden ${selectedData.id?"bottom-0":"-bottom-[550px]"}`}>
+              <div className='  px-3 p-2 space-y-3.5 h-fit mt-1 w-5xl text-secondary-green '>
                 <h3 className=' text-2xl font-semibold mb-2'>Your Selection</h3>
                 <div className=' flex gap-2 flex-col mid-mobile:flex-row mid-mobile:justify-between'>
-                  <div className=' flex gap-10 mid-mobile:flex-col sm:flex-row'>
+                  <div className=' flex gap-0.5 sm:gap-10 mid-mobile:flex-col sm:flex-row'>
                     <div>
                       <p><strong>Size:</strong> {selectedData.size} yard skip</p>
                       <p><strong>Price before VAT:</strong> £{selectedData.price_before_vat}</p>
@@ -46,6 +47,10 @@ function Footer({selectedData}:{selectedData:SkipData}) {
 
                   </div>
                   <img className='   mid-mobile:h-28' src={selectedData.size<8?small_skip:selectedData.size<12?light_medium_skip:selectedData.size<16?medium_skip:selectedData.size<40?large_skip:heavy_skip} alt="" />
+                </div>
+                <div className="flex items-start gap-1">
+                    <BsInfoCircleFill size={24} className=" animate-pulse" />
+                    <p className=" text-xs ">Imagery and information shown throughout this website may not reflect the exact shape or size specification, colours may vary, options and/or accessories may be featured at additional cost.</p>
                 </div>
                 <div className=' flex gap-2.5 justify-end-safe font-medium'>
                   <button className=' bg-background-green rounded-xl px-8 py-1.5 hover:bg-background-green/50 hover:cursor-pointer'>Back</button>
